4e303089d436d29421476ed74cc6fc1904b0c5cf
paptreek <paptreek@hotmail.com>
2026-01-07 21:27:39 -0600

    Refactoring some redundant stuff out, still in progress

--- GameBoard.cs
+++ GameBoard.cs
@@ -11,9 +11,21 @@ public class GameBoard
     private float _boardScale = 8.0f;
     private SpriteEffects _boardEffects = SpriteEffects.None;
     private float _boardLayerDepth = 0.0f;
+    
+    private Rectangle[] _bounds = [
+        new Rectangle(8, 8, 120, 120),
+        new Rectangle(144, 8, 112, 120),
+        new Rectangle(272, 8, 120, 120),
+        new Rectangle(8, 144, 120, 112),
+        new Rectangle(144, 144, 112, 112),
+        new Rectangle(272, 144, 120, 112),
+        new Rectangle(8, 272, 120, 120),
+        new Rectangle(144, 272, 112, 120),
+        new Rectangle(272, 272, 120, 120)
+    ];
+
     public Vector2 BoardPosition { get; private set; }
     public Rectangle BoardSource { get; private set; }
-
     public bool IsTopLeftTaken { get; set; }
     public bool IsTopMiddleTaken { get; set; }
     public bool IsTopRightTaken { get; set; }
@@ -55,6 +67,42 @@ public class GameBoard
     public void Draw(SpriteBatch spriteBatch, Vector2 boardOrigin)
     {
         spriteBatch.Draw(_boardTexture, BoardPosition, BoardSource, _boardColor, _boardRotation, boardOrigin, _boardScale, _boardEffects, _boardLayerDepth);
+    }
+
+    public Rectangle GetBounds(BoardLocation boardLocation)
+    {
+        Rectangle location = boardLocation switch
+        {
+            BoardLocation.TopLeft   => _bounds[0],
+            BoardLocation.TopMiddle => _bounds[1],
+            BoardLocation.TopRight  => _bounds[2],
+            BoardLocation.Left   => _bounds[3],
+            BoardLocation.Middle => _bounds[4],
+            BoardLocation.Right  =>  _bounds[5],
+            BoardLocation.BottomLeft   => _bounds[6],
+            BoardLocation.BottomMiddle => _bounds[7],
+            BoardLocation.BottomRight  => _bounds[8]
+        };
+
+        return location;
+    }
+
+    public Vector2 GetLocation(BoardLocation boardLocation)
+    {
+        Vector2 location = boardLocation switch
+        {
+            BoardLocation.TopLeft   => new Vector2(_bounds[0].X + 60, _bounds[0].Y + 62),
+            BoardLocation.TopMiddle => new Vector2(_bounds[1].X + 56, _bounds[1].Y + 62),
+            BoardLocation.TopRight  => new Vector2(_bounds[2].X + 60, _bounds[2].Y + 62),
+            BoardLocation.Left   => new Vector2(_bounds[3].X + 60, _bounds[3].Y + 56),
+            BoardLocation.Middle => new Vector2(_bounds[4].X + 56, _bounds[4].Y + 56),
+            BoardLocation.Right  => new Vector2(_bounds[5].X + 60, _bounds[5].Y + 56),
+            BoardLocation.BottomLeft   => new Vector2(_bounds[6].X + 60, _bounds[6].Y + 62),
+            BoardLocation.BottomMiddle => new Vector2(_bounds[6].X + 56, _bounds[6].Y + 62),
+            BoardLocation.BottomRight  => new Vector2(_bounds[6].X + 60, _bounds[6].Y + 62),
+        };
+
+        return location;
     }
 
     public Rectangle GetTopLeft()
@@ -156,3 +204,5 @@ public class GameBoard
         return new Vector2(GetBottomRight().X + 60, GetBottomRight().Y + 62);
     }
 }
+
+public enum BoardLocation { TopLeft, TopMiddle, TopRight, Left, Middle, Right, BottomLeft, BottomMiddle, BottomRight }
